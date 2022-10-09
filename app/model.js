2e2167f23e5b4b5f4bc5d53e539f37f0deccb479
Keegan Briskey <70216437+kbriske@users.noreply.github.com>
2022-10-09 10:33:56 -0400

    set model back to books

--- app/model.js
+++ app/model.js
@@ -187,7 +187,7 @@ export function changePage(pageID, callback) {
       callback();
       
     });
-  } else if (pageID == "books2") {
+  } else if (pageID == "books") {
     $.get(`pages/${pageID}.html`, function (data) {
       //   console.log("data " + data);
       $("#app").html(data);
